79090cd57265584ab8388ac2cd39442b19cbcff6
Sujeet Kumar <sujeet901626@gmail.com>
2026-06-11 06:29:44 +0530

    Create A - Armor.cpp

---
+++ ABC461/A - Armor.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define sp ' '
+#define pb push_back
+#define inf 1e18
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int N, D;
+  cin >> N >> D;
+  if (N > D)
+    cout << "No" << endl;
+  else 
+    cout << "Yes" << endl;
+  return 0;
+}
